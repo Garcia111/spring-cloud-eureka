61c19fca5264669bbce8396d019adfb4a1af09ce
chengyanxiao <chengyanxiao@ctsi.com.cn>
2020-04-07 23:02:11 +0800

    整合Config Server

--- src/main/java/springcloudeureka/demo/DemoApplication.java
+++ src/main/java/springcloudeureka/demo/DemoApplication.java
@@ -2,8 +2,10 @@ package springcloudeureka.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@EnableDiscoveryClient
 @EnableEurekaServer
 @SpringBootApplication
 public class DemoApplication {
